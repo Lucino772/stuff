dde35bd2ffbe46ff206f6d91d5bdb1edcdece0b2
Lucino772 <lucapalmi772@gmail.com>
2022-12-23 10:00:17 +0100

    fix(ohmyposh): updated the powershell profile to use local theme

--- dotfiles/powershell/profile.ps1
+++ dotfiles/powershell/profile.ps1
@@ -1,5 +1,6 @@
 # Disable prompt for virtual environment (python)
 # ! for conda use the .condarc file and set env_prompt: ''
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
+$env:OHMYPOSH_THEME = '~/.ohmyposh_themes/lucino772.omp.json'
 
-oh-my-posh init pwsh --config "https://raw.githubusercontent.com/Lucino772/stuff/main/dotfiles/ohmyposh/lucino772.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:OHMYPOSH_THEME" | Invoke-Expression
